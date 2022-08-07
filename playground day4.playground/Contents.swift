398bdf2070a77d7b9fdcc738c89b7791b757e3df
Nddakhil <109868614+Nddakhil@users.noreply.github.com>
2022-08-07 15:11:30 +0300

    text added

---
+++ playground day4.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var name = "Noor"
+var birthyear = 2005
+var age : Int
+var currentyear = 2022
+
+age = currentyear - birthyear
+
+//Hello my name is your name and my age is your
+
+print("Hello my name is \(name) and my age is \(age)")
+
+ if age >= 14 && age <= 18
+{
+ print("مبروك القبول")
+ }
+else{
+
+  print("جرب سنه ثانيه")
+}
+var textName = "8"
+var number = 4
+var doubleNumb : Double
+
+doubleNumb = (Double(textName) ?? 0) + Double(number)
+ 
+ 
